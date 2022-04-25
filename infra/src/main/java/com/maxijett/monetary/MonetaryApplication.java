8865ac7c8ec3589f80f023a5e34ba3753f3a5a6b
davut.keskin <davut.keskin@cloudnesil.com>
2022-04-25 16:27:39 +0300

    initial commit

---
+++ infra/src/main/java/com/maxijett/monetary/MonetaryApplication.java
@@ -0,0 +1,11 @@
+package com.maxijett.monetary;
+
+import org.springframework.boot.SpringApplication;
+
+public class MonetaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonetaryApplication.class, args);
+    }
+
+}
