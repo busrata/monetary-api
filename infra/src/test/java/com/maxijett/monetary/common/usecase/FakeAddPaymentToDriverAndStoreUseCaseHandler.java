988747e1efc16fc325b49759721bf82c84c287c7
davutkeskin <davut.keskin@cloudnesil.com>
2022-09-06 16:28:22 +0300

    use void UseCaseHandler on test and implementation

--- infra/src/test/java/com/maxijett/monetary/common/usecase/FakeAddPaymentToDriverAndStoreUseCaseHandler.java
+++ infra/src/test/java/com/maxijett/monetary/common/usecase/FakeAddPaymentToDriverAndStoreUseCaseHandler.java
@@ -3,10 +3,9 @@ package com.maxijett.monetary.common.usecase;
 import com.maxijett.monetary.orderpayment.useCase.OrderPayment;
 
 
-public class FakeAddPaymentToDriverAndStoreUseCaseHandler implements UseCaseHandler<Boolean, OrderPayment> {
+public class FakeAddPaymentToDriverAndStoreUseCaseHandler implements VoidUseCaseHandler<OrderPayment> {
 
-  @Override
-  public Boolean handle(OrderPayment useCase) {
-    return true;
-  }
+    @Override
+    public void handle(OrderPayment useCase) {
+    }
 }
